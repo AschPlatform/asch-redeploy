4b15e152316f9dac85468f74a1c8510190d2cc88
a1300 <matthias-knopp@gmx.net>
2018-05-17 13:29:28 +0200

    trying to restart asch-node

--- deploy.js
+++ deploy.js
@@ -2,6 +2,7 @@ let aschJS = require('asch-js')
 let fs = require('fs')
 let path = require('path')
 let axios = require('axios')
+let ncp = require('ncp').ncp
 
 // constructor
 let deploy = function (config) {
@@ -22,7 +23,7 @@ let deploy = function (config) {
     let trs = aschJS.dapp.createDApp(dapp, secret, secondSecret)
 
     let url = `${config.host}:${config.port}/peer/transactions`
-    axios({
+    return axios({
       method: 'POST',
       url: url,
       headers: {
@@ -32,29 +33,43 @@ let deploy = function (config) {
       data: {
         transaction: trs
       }
-    }).then(function (response) {
-      if (response.data.success === true) {
-        callback(null, response.data)
-      } else {
-        callback(null, response.data.error)
-      }
-    }).catch(function (err) {
-      callback(err)
     })
   } // deploy end
 
-  this.copyFiles = function (dappId, callback) {
-    let newDappDirectory = path.join(this.config.asch, 'dapp', dappid)
-    fs.mkdirSync(newDappDirectory)
+  this.copyFiles = function (dappId) {
+    let self = this
+    return new Promise(function (resolve, reject) {
+      let dappParentDir = path.join(self.config.asch, 'dapps')
+      let existsDappDir = fs.existsSync(dappParentDir)
+      if(existsDappDir === false) {
+        fs.mkdirSync(dappParentDir)
+      }
 
-    let dappConfigPath = path.join(this.config.executionDir,  'config.json')
-    let dappConfig = JSON.parse(fs.readFileSync(dappConfigPath, 'utf8'))
-
-    dappConfig.secret.push(this.config.delegates)
-    fs.writeFileSync(dappConfigPath, dappConfig, 'utf8')
+      let newDappDirectory = path.join(self.config.asch, 'dapps', dappId)
+      fs.mkdirSync(newDappDirectory)
+  
+      let dappConfigPath = path.join(self.config.executionDir,  'config.json')
+      let dappConfig = JSON.parse(fs.readFileSync(dappConfigPath, 'utf8'))
+  
+      dappConfig.secrets.push(...self.config.delegates)
+      fs.writeFileSync(dappConfigPath, JSON.stringify(dappConfig), 'utf8')
+  
+      ncp(self.config.executionDir, newDappDirectory, function (err) {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(null)
+        }
+      })
+    })
 
     
-    callback()
+  } // end copyFiles
+
+  this.restartAsch = function () {
+    return new Promise(function (resolve, reject) {
+      
+    })
   }
 }
 
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const config = require('config')
 const path = require('path')
 const watch = require('node-watch')
 const deploy = require('./deploy')
+var Service = require('node-linux').Service;
 
 // config
 let executionDir = path.dirname(require.main.filename)
@@ -16,16 +17,43 @@ watch(executionDir, { recursive: true }, function (evt, name) {
   console.log(name)
 })
 
-
 // register
 let dep = new deploy(defaultConfig)
-dep.registerDapp(function (error, transactionId) {
-  if (error) {
-    return
-  }
 
-  dep.copyFiles(transactionId, function (err, result) {
+dep.registerDapp()
+  .then(function (response) {
+    if (response.status !== 200) {
+      throw new Error('Could not register dapp')
+    }
+    if (response.data.success === false) {
+      throw new Error(response.data.error)
+    }
+    console.log(`dappId: ${response.data.transactionId}`)
+    return dep.copyFiles(response.data.transactionId)
+  })
+  .then(function (result) {
+
+    let pathToService = path.join(defaultConfig.asch, 'aschd')
+    let svc = new Service({
+      name:'Asch Service',
+      script: pathToService
+    })
+
+    svc.on('alreadyinstalled', function (){
+
+    })
+    svc.on('start', function () {
+
+    })
+    svc.on('stop', function () {
+
+    })
+    svc.on('doesnotexist', function () {
+
+    })
+
 
   })
-})
-
+  .catch(function(error) {
+    console.log(error)
+  })
