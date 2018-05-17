f80c35026760288fac63dc82a9ba7566ccd82bdb
a1300 <matthias-knopp@gmx.net>
2018-05-17 11:07:45 +0200

    update3

--- deploy.js
+++ deploy.js
@@ -7,17 +7,21 @@ let axios = require('axios')
 let deploy = function (config) {
   this.config = config
 
+  this.enoughMoney = function () {
+
+  }
+
   this.registerDapp = function (callback) {
     let secret = this.config.dappMasterAccountPassword
     let secondSecret = this.config.dappMasterAccountPassword2nd
   
     let dappJsFile = path.join(config.executionDir, 'dapp.json')
+    // todo check if file exists
     var dapp = JSON.parse(fs.readFileSync(dappJsFile, 'utf8'));
-  
-    let trs = aschJS.dapp.createDApp(dapp, secret, options.secondSecret)
-    
+    let trs = aschJS.dapp.createDApp(dapp, secret, secondSecret)
+
     let url = `${config.host}:${config.port}/peer/transactions`
-    axios.post(url, trs, function (err, result) {
+    axios.post(url, trs, { headers: { magic: this.config.magic, version: '' }}, function (err, result) {
       if (result.status === 200 && result.data.success === true) {
         callback(null, result.data.transactionId)
       } else {
