55a70da1e674777376ca2fba17b2f41b175e23cf
a1300 <matthias-knopp@gmx.net>
2018-05-19 17:04:52 +0200

    ending child-processes with SIGTERM works

--- index.js
+++ index.js
@@ -7,30 +7,9 @@ const log = console.log
 const deploy = require('./src/deploy')
 const service = require('./src/asch-service')
 
-
-// https://www.exratione.com/2013/05/die-child-process-die/
-process.on('SIGTERM', function () {
-  // kill
-  log(chalk.blue('SIGTERM'))
-  aschService.execute('stop')
-  .then(function (result) {
-    log(chalk.blue(result))
-    process.exit(0)
-  })
-})
-process.on('SIGINT', function () {
-  // ctrl+c
-  log(chalk.blue('SIGTERM'))
-  aschService.execute('stop')
-    .then(function (result) {
-      log(chalk.blue(result))
-      process.exit(0)
-    })
-})
-process.once("uncaughtException", function (error) {
-  log(chalk.red('UNCAUGHT EXCEPTION'))
-  log(error)
-})
+if (process.platform !== 'linux') {
+  log(chalk.red('This program can currently run only on linux'))
+}
 
 
 // config
@@ -41,61 +20,88 @@ defaultConfig.userDevDir = userDevDir
 
 log(chalk.red(`You started "asch-redeploy" from directory "${userDevDir}"`))
 
+
+// https://www.exratione.com/2013/05/die-child-process-die/
+process.on('SIGTERM', function () {
+  // kill
+  log(chalk.blue('SIGTERM'))
+  aschService.stop()
+  process.exit(0)
+})
+process.on('SIGINT', function () {
+  // ctrl+c
+  log(chalk.blue('SIGTERM'))
+  aschService.stop()
+  process.exit(0)
+})
+process.once("uncaughtException", function (error) {
+  log(chalk.red('UNCAUGHT EXCEPTION'))
+  log(error)
+})
+
+
 let aschService = new service(defaultConfig.node.directory)
 
+// start asch node
 aschService.start()
-log(chalk.red('AFTER START'))
 
 aschService.notifier.on('exit', function (code){
   console.log(`aschService terminated with code ${code}`)
+  process.exit(code)
 })
 
-setTimeout(function () {
-  log(chalk.red('CALLING STOP'))
-  aschService.stop()
-}, 2000)
+let dep = new deploy(defaultConfig)
 
-// let dep = new deploy(defaultConfig)
-
-
-  // .then(function startServer(result) {
-  //   log(chalk.green(result))
-  //   return dep.sendMoney()
-  // })
-  // .then(function sendMoneyFinished(response) {
-  //   if (response.status !== 200) {
-  //     Promise.reject('Could not send money')
-  //   }
-  //   if (response.data.success === false) {
-  //     Promise.reject(response.data.error)
-  //   }
-  //   log(chalk.green(`successful created money transaction: ${response.data.transactionId}`))
-  //   return null
-  // })
-  // .then(function timeOutAfterMoneyTransfer() {
-  //   return new Promise(function (resolve, reject) {
-  //     setTimeout(function () {
-  //       resolve(dep.registerDapp())
-  //     }, 10000)
-  //   })
-  // })
-  // .then(function registerDappFinished(response) {
-  //   if (response.status !== 200) {
-  //     throw new Error('Could not register dapp')
-  //   }
-  //   if (response.data.success === false) {
-  //     throw new Error(response.data.error)
-  //   }
-  //   log(chalk.green(`\nDAPP registered, DappId: ${response.data.transactionId}\n`))
-  //   return dep.copyFiles(response.data.transactionId)
-  // })
-  // .then(function copyingFilesFinished(result) {
-  //   return aschService.execute('restart')
-  // })
-  // .then (function restartResult(result) {
-  //   log(chalk.green(result))
-  // })
-  // .catch(function errorOccured(error) {
-  //   log(chalk.red('ERROR OCCURED'))
-  //   log(chalk.red(error.message))
-  // })
+dep.sendMoney()
+  .then(function sendMoneyFinished(response) {
+    if (response.status !== 200) {
+      Promise.reject('Could not send money')
+    }
+    if (response.data.success === false) {
+      Promise.reject(response.data.error)
+    }
+    log(chalk.green(`successful created money transaction: ${response.data.transactionId}`))
+    return null
+  })
+  .then(function timeOutAfterMoneyTransfer() {
+    return new Promise(function (resolve, reject) {
+      setTimeout(function () {
+        resolve(dep.registerDapp())
+      }, 10000)
+    })
+  })
+  .then(function registerDappFinished(response) {
+    if (response.status !== 200) {
+      throw new Error('Could not register dapp')
+    }
+    if (response.data.success === false) {
+      throw new Error(response.data.error)
+    }
+    log(chalk.green(`\nDAPP registered, DappId: ${response.data.transactionId}\n`))
+    return dep.copyFiles(response.data.transactionId)
+  })
+  .then(function copyingFilesFinished(result) {
+    console.log(result)
+    aschService.stop()
+    return new Promise(function (resolve, reject) {
+      setTimeout(function timeout() {
+        resolve('aschService stopped')
+      }, 5000)
+    })
+  })
+  .then(function aschServerStoppedFinished(result){
+    console.log(result)
+    aschService.start()
+    return new Promise(function (resolve, reject) {
+      setTimeout(function timeout() {
+        resolve('aschService started')
+      }, 5000)
+    })
+  })
+  .then (function restartResult(result) {
+    log(chalk.green(result))
+  })
+  .catch(function errorOccured(error) {
+    log(chalk.red('ERROR OCCURED'))
+    log(chalk.red(error.message))
+  })
