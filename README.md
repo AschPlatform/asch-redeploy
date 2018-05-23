4cd4cffb81752ad29670904b6b3397090ae5dad4
a1300 <matthias-knopp@gmx.net>
2018-05-23 12:12:25 +0200

    linted new file

--- README.md
+++ README.md
@@ -21,10 +21,6 @@ asch-redeploy .
 * Only watch on some folders and filetypes
 * retry when not enough money
 * BUG: Restart didn't worked! (after dapp registration)
-* SendMoney move to own file
-  * Send only money, when not enough money (this way is faster)
-  * 
-* Integrate chalk package for colorful console-output
 * Do I need to create a softlink in asch/public/dapps? (I think the asch-binaries should be built before)
     * Softlink in /home/ma/asch/public/dist/dapps/a7ef9cd0707fe94f583334d434e39570113a43e3647c8ee730bf21ec2fe08c97
     * points to /home/ma/asch/dapps/a7ef9cd0707fe94f583334d434e39570113a43e3647c8ee730bf21ec2fe08c97/public
@@ -38,5 +34,5 @@ ln -s /home/matt/test/asch/dapps/efc789be7881c014773629eb955e527367d247e2885d6f0
 # Contributing
 If the following error displays:
  `Error: listen EADDRINUSE 0.0.0.0:4096`, this means the the asch-node is already running and must be terminated.
- 
+
 `fuser --kill 4096/tcp`
--- src/fileStructureExists.js
+++ src/fileStructureExists.js
@@ -4,40 +4,39 @@ const fs = require('fs')
 // ctor
 let exists = function (userDevDir) {
   if (!(typeof userDevDir === 'string')) {
-    throw new Exception('param userDevDir must be of type string')
+    throw new Error('param userDevDir must be of type string')
   }
   this.userDevDir = userDevDir
-
 
   this.check = function () {
     let self = this
     return new Promise(function (resolve, reject) {
       let contractDir = path.join(self.userDevDir, 'contract')
       if (!fs.existsSync(contractDir)) {
-        Promise.reject('contract directory doesn\'t exist')
+        Promise.reject(new Error('contract directory doesn\'t exist'))
       }
-      
+
       let interfaceDir = path.join(self.userDevDir, 'interface')
       if (!fs.existsSync(interfaceDir)) {
-        Promise.reject('interface directory doesn\'t exist')
+        Promise.reject(new Error('interface directory doesn\'t exist'))
       }
 
       let modelDir = path.join(self.userDevDir, 'model')
       if (!fs.existsSync(modelDir)) {
-        Promise.reject('model directory doesn\'t exist')
+        Promise.reject(new Error('model directory doesn\'t exist'))
       }
 
       let publicDir = path.join(self.userDevDir, 'public')
       if (!fs.existsSync(publicDir)) {
-        Promise.reject('public directory doesn\'t exist')
+        Promise.reject(new Error('public directory doesn\'t exist'))
       }
 
       let initFile = path.join(self.userDevDir, 'init.js')
       if (!fs.existsSync(initFile)) {
-        Promise.reject('init.js file doesn\'t exist')
+        Promise.reject(new Error('init.js file doesn\'t exist'))
       }
 
-      Promse.resolve(true)
+      Promise.resolve(true)
     })
   }
 }
