0ad6dabae0bd2bc88dead2c8607642d8b7ef767f
a1300 <matthias-knopp@gmx.net>
2018-05-25 10:42:36 +0200

    bip39 validator seems to work

--- src/customValidators.js
+++ src/customValidators.js
@@ -1,13 +1,20 @@
 const ZSchema = require('z-schema')
 const Mnemonic = require('bitcore-mnemonic')
 const fs = require('fs')
+const chalk = require('chalk')
+const log = console.log
 
 ZSchema.registerFormat('bip39', (secret) => {
-  console.log(secret)
-  return Mnemonic.isValid(secret)
+  let isBip39 = false
+  try {
+    isBip39 = Mnemonic.isValid(secret)
+  } catch (err) {
+    log(chalk.red(`secret "${secret}" is not bip39 complient`))
+    log(chalk.red(err.message))
+    throw err
+  }
+  return isBip39
 })
-
-// achtung type is nicht format!!!
 
 ZSchema.registerFormat('file', (path) => {
   console.log(path)
