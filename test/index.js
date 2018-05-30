533ea64f7dec93142047a7897b3da7ed2d666447
a1300 <matthias-knopp@gmx.net>
2018-05-30 20:55:41 +0200

    no error "describe is not defined" on test/index.js

--- test/index.js
+++ test/index.js
@@ -1,8 +1,9 @@
+/* global describe it */
 const Deploy = require('../src/orchestration/deploy')
 const assert = require('assert')
 
 describe('', function () {
-  it ('works', function () {
+  it('works', function () {
     let deploy = new Deploy()
     assert.ok(deploy)
   })
