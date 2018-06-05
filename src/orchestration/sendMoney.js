a05644bc0ea1c519baa4cccd008e2533da6e0e76
a1300 <matthias-knopp@gmx.net>
2018-06-05 11:01:59 +0200

    use global Container for sendMoney.js

--- src/orchestration/sendMoney.js
+++ src/orchestration/sendMoney.js
@@ -1,18 +1,14 @@
-const axios = require('axios')
-const aschJS = require('asch-js')
-const Promise = require('bluebird')
-const logger = require('../logger')
 
 // ctor
 let sendMoney = function (config) {
   this.config = config
   let toSecret = this.config.dapp.masterAccountPassword
-  let toAddress = aschJS.crypto.getAddress(aschJS.crypto.getKeys(toSecret).publicKey)
+  let toAddress = container.aschJS.crypto.getAddress(container.aschJS.crypto.getKeys(toSecret).publicKey)
 
   this.getBalance = function () {
     let self = this
     let url = `${self.config.node.host}:${self.config.node.port}/api/accounts/getBalance`
-    return axios.get(url,
+    return container.axios.get(url,
       {
         params: {
           address: toAddress
@@ -24,7 +20,7 @@ let sendMoney = function (config) {
   this.enoughMoney = function (response) {
     let min = 1000
     if (response.status === 200 && response.data.success === true && (response.data.balance / 1e8) >= min) {
-      logger.verbose(`enough money on account. No transfer needed. Balance is ${response.data.balance}`)
+      container.logger.verbose(`enough money on account. No transfer needed. Balance is ${response.data.balance}`)
       throw new Error('enough_money')
     } else {
       return null
@@ -35,7 +31,7 @@ let sendMoney = function (config) {
     let amount = 20000
     let genesisSecret = config.node.genesisAccount
 
-    var trs = aschJS.transaction.createTransaction(
+    var trs = container.aschJS.transaction.createTransaction(
       toAddress,
       Number(amount * 1e8),
       null,
@@ -59,12 +55,12 @@ let sendMoney = function (config) {
 
   this.handleTransferResponse = function (response) {
     if (response.status !== 200) {
-      Promise.reject(new Error('Could not send money'))
+      container.Promise.reject(new Error('Could not send money'))
     }
     if (response.data.success === false) {
-      Promise.reject(new Error(response.data.error))
+      container.Promise.reject(new Error(response.data.error))
     }
-    logger.verbose(`successful created money transaction: ${response.data.transactionId}`)
+    container.logger.verbose(`successful created money transaction: ${response.data.transactionId}`)
     return null
   } // handleTransfer
 
