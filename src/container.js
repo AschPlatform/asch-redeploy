548255dc3047db13a6b4ba8229abe910ed37b829
a1300 <matthias-knopp@gmx.net>
2018-06-06 09:52:15 +0200

    inversify seems to work

--- src/container.js
+++ src/container.js
@@ -5,11 +5,14 @@ const helpers = require('inversify-vanillajs-helpers').helpers
 require('reflect-metadata')
 
 const SendMoney = require('./orchestration/sendMoney')
-
-
 const FILETYPES = {
   SendMoney: 'SendMoney'
 }
+
+const Axios = require('axios')
+const Logger = require('./logger')
+const AschJS = require('asch-js')
+const Promise = require('bluebird')
 
 const DEPENDENCIES = {
   Config: 'Config',
@@ -20,80 +23,33 @@ const DEPENDENCIES = {
 }
 
 
+helpers.annotate(SendMoney, [DEPENDENCIES.Config, DEPENDENCIES.Logger, DEPENDENCIES.Axios, DEPENDENCIES.AschJS, DEPENDENCIES.Promise])
+
 // register config
 let Config = require('config').config
-console.log(`config: ${JSON.stringify(Config)}`)
-
-
 var container = new inversify.Container()
 
+container.bind(FILETYPES.SendMoney).to(SendMoney)
 
-var registerConstantValue = helpers.registerConstantValue(container);
+
+const registerConstantValue = helpers.registerConstantValue(container);
 registerConstantValue(DEPENDENCIES.Config, Config)
 
-
-
-// TODO Annotate the dependencies
-
-// config, logger, axios, aschJS, promise
-// helpers.annotate(SendMoney, [DEPENDENCIES.Config, DEPENDENCIES.Logger, DEPENDENCIES.Axios, DEPENDENCIES.AschJS, DEPENDENCIES.Promise])
-
-let resolvedConfig = container.get(DEPENDENCIES.Config)
-console.log(`\nresolvedConfig: ${JSON.stringify(resolvedConfig)}`)
-
-
-process.exit(0)
+registerConstantValue(DEPENDENCIES.Logger, Logger)
+registerConstantValue(DEPENDENCIES.Axios, Axios)
+registerConstantValue(DEPENDENCIES.AschJS, AschJS)
+registerConstantValue(DEPENDENCIES.Promise, Promise)
 
 
 
-var TYPES = {
-  Ninja: 'Ninja',
-  Katana: 'Katana',
-  Shuriken: 'Shuriken'
-}
 
 
-const Katana = function () {
-  this.hit = function () {
-    return 'cut!'
-  }
-}
-helpers.annotate(Katana)
+// const registerConstructor = helpers.registerConstructor(container);
+// registerConstructor(FILETYPES.SendMoney)(SendMoney)
 
 
-const Shuriken = function () {
-  this.throw = function () {
-    return 'hit!'
-  }
-}
-
-helpers.annotate(Shuriken);
+let axi =container.get(DEPENDENCIES.Axios)
+console.log(`logger: ${axi}`)
 
 
-const Ninja = function (katana, shuriken) {
-  this._katana = katana;
-  this._shuriken = shuriken;
-
-  this.fight = function () {
-    return this._katana.hit() 
-  }
-
-  this.sneak = function () {
-    return this._shuriken.throw()
-  }
-}
-
-helpers.annotate(Ninja, [TYPES.Katana, TYPES.Shuriken]);
-
-// Declare bindings
-var container = new inversify.Container()
-container.bind(TYPES.Ninja).to(Ninja);
-container.bind(TYPES.Katana).to(Katana);
-container.bind(TYPES.Shuriken).to(Shuriken);
-
-// Resolve dependencies
-var ninja = container.get(TYPES.Ninja);
-console.log(ninja.fight(), ninja.sneak());
-
-let shiru = container.get(TYPES.Shuriken)
-console.log(shiru.throw())
+module.exports = container
