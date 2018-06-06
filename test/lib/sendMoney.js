8a53e9145d952890450c7a13d47f78548e450b59
a1300 <matthias-knopp@gmx.net>
2018-06-06 12:15:16 +0200

    restructure sendMoney tests

--- test/lib/sendMoney.js
+++ test/lib/sendMoney.js
@@ -25,123 +25,178 @@ describe('sendMoney', function () {
     DI.resetConstants()
   })
 
-  it('injection worked', function (done) {
-    let sendMoney = DI.container.get(DI.FILETYPES.SendMoney)
+  describe('happy path', function () {
+    it('injection worked', function (done) {
+      let sendMoney = DI.container.get(DI.FILETYPES.SendMoney)
 
-    should(sendMoney).be.ok()
-    should(sendMoney).have.property('config')
-    should(sendMoney).have.property('logger')
-    should(sendMoney).have.property('axios')
-    should(sendMoney).have.property('aschJS')
-    should(sendMoney).have.property('Promise')
-    done()
-  })
+      should(sendMoney).be.ok()
+      should(sendMoney).have.property('config')
+      should(sendMoney).have.property('logger')
+      should(sendMoney).have.property('axios')
+      should(sendMoney).have.property('aschJS')
+      should(sendMoney).have.property('Promise')
+      done()
+    })
 
-  it('zero balance on genesis-account exits with error', function (done) {
-    // config
-    const Axios = {
-      post (url, config) {
-        return new Promise((resolve, reject) => {
-          let result = {
-            status: 200,
-            data: {
-              success: true,
-              account: {
-                balance: 0,
-                address: '14762548536863074694'
-              }
-            }
-          }
+    it('zero balance on dapp-master-account starts money-transfer and returns transactionId', function (done) {
+      // config
+      let transactionId = 'i300gho34h0hgyxghzzwzdbyzHB34'
+      const Axios = {
+        called: 0,
+        post (url, config) {
+          return new Promise((resolve, reject) => {
+            this.called += 1
 
-          resolve(result)
-        })
-      }
-    }
-
-    DI.container.unbind(DI.DEPENDENCIES.Axios)
-    registerConstant(DI.DEPENDENCIES.Axios, Axios)
-
-    /* act */
-    let sendMoney = DI.container.get(DI.FILETYPES.SendMoney)
-    sendMoney.sendMoney()
-      .then((result) => {
-        console.log(`result: ${result}`)
-      })
-      .then(() => {
-        throw new Error('should throw exception')
-      })
-      .catch((error) => {
-        should(error).has.property('message').startWith('genesisAccount_empty')
-        done()
-      })
-  })
-
-  it('zero balance on dapp-master-account cause money transfer and returns transactionId', function (done) {
-    // config
-    let transactionId = 'i300gho34h0hgyxghzzwzdbyzHB34'
-    const Axios = {
-      called: 0,
-      post (url, config) {
-        return new Promise((resolve, reject) => {
-          this.called += 1
-
-          let result = {}
-          if (this.called === 1) {
-            result = {
-              status: 200,
-              data: {
-                success: true,
-                account: {
-                  balance: 100000 * 1e8,
-                  address: '14762548536863074694'
+            let result = {}
+            if (this.called === 1) {
+              result = {
+                status: 200,
+                data: {
+                  success: true,
+                  account: {
+                    balance: 100000 * 1e8,
+                    address: '14762548536863074694'
+                  }
                 }
               }
             }
-          }
-          if (this.called === 2) {
-            result = {
+            if (this.called === 2) {
+              result = {
+                status: 200,
+                data: {
+                  success: true,
+                  account: {
+                    balance: 0,
+                    address: 'AHMCKebuL2nRYDgszf9J2KjVZzAw95WUyB'
+                  }
+                }
+              }
+            }
+            if (this.called === 3) {
+              result = {
+                status: 200,
+                data: {
+                  success: true,
+                  transactionId: transactionId
+                }
+              }
+            }
+
+            resolve(result)
+          })
+        }
+      }
+
+      DI.container.unbind(DI.DEPENDENCIES.Axios)
+      registerConstant(DI.DEPENDENCIES.Axios, Axios)
+
+      /* act */
+      let sendMoney = DI.container.get(DI.FILETYPES.SendMoney)
+      sendMoney.sendMoney()
+        .then((result) => {
+          should(result).equals(transactionId)
+          done()
+        })
+        .catch((error) => {
+          throw error
+        })
+    })
+
+    it('already charged dapp-master-account returns "enough_money" message', function (done) {
+      // config
+      const Axios = {
+        called: 0,
+        post (url, config) {
+          return new Promise((resolve, reject) => {
+            this.called += 1
+
+            let result = {}
+            if (this.called === 1) {
+              result = {
+                status: 200,
+                data: {
+                  success: true,
+                  account: {
+                    balance: 100000 * 1e8,
+                    address: '14762548536863074694'
+                  }
+                }
+              }
+            }
+            if (this.called === 2) {
+              result = {
+                status: 200,
+                data: {
+                  success: true,
+                  account: {
+                    balance: 50000 * 1e8,
+                    address: 'AHMCKebuL2nRYDgszf9J2KjVZzAw95WUyB'
+                  }
+                }
+              }
+            }
+
+            resolve(result)
+          })
+        }
+      }
+
+      DI.container.unbind(DI.DEPENDENCIES.Axios)
+      registerConstant(DI.DEPENDENCIES.Axios, Axios)
+
+      /* act */
+      let sendMoney = DI.container.get(DI.FILETYPES.SendMoney)
+      sendMoney.sendMoney()
+        .then((result) => {
+          throw new Error(result)
+        })
+        .catch((error) => {
+          should(error.message).equals('enough_money')
+          done()
+        })
+    })
+  })
+
+  describe('sad path', function () {
+    it('zero balance on genesis-account exits with error', function (done) {
+      // config
+      const Axios = {
+        post (url, config) {
+          return new Promise((resolve, reject) => {
+            let result = {
               status: 200,
               data: {
                 success: true,
                 account: {
                   balance: 0,
-                  address: 'AHMCKebuL2nRYDgszf9J2KjVZzAw95WUyB'
+                  address: '14762548536863074694'
                 }
               }
             }
-          }
-          if (this.called === 3) {
-            result = {
-              status: 200,
-              data: {
-                success: true,
-                transactionId: transactionId
-              }
-            }
-          }
 
-          resolve(result)
-        })
+            resolve(result)
+          })
+        }
       }
-    }
 
-    DI.container.unbind(DI.DEPENDENCIES.Axios)
-    registerConstant(DI.DEPENDENCIES.Axios, Axios)
+      DI.container.unbind(DI.DEPENDENCIES.Axios)
+      registerConstant(DI.DEPENDENCIES.Axios, Axios)
 
-    /* act */
-    let sendMoney = DI.container.get(DI.FILETYPES.SendMoney)
-    sendMoney.sendMoney()
-      .then((result) => {
-        should(result).equals(transactionId)
-        done()
-      })
-      .catch((error) => {
-        throw error
-      })
+      /* act */
+      let sendMoney = DI.container.get(DI.FILETYPES.SendMoney)
+      sendMoney.sendMoney()
+        .then((result) => {
+          console.log(`result: ${result}`)
+        })
+        .then(() => {
+          throw new Error('should throw exception')
+        })
+        .catch((error) => {
+          should(error).has.property('message').startWith('genesisAccount_empty')
+          done()
+        })
+    })
+
+    it.skip('network error exists with exception')
   })
-
-  it.skip('charged dapp-master-account returns "enough_money" message', function () {
-  })
-
-  it.skip('network error exists with exception')
 })
