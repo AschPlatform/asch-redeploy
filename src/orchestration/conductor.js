0e963c421c49f4ae102b9a41c53af9d03a807d46
a1300 <matthias-knopp@gmx.net>
2018-06-26 23:08:52 +0200

    Register dapp on first start

--- src/orchestration/conductor.js
+++ src/orchestration/conductor.js
@@ -8,14 +8,23 @@ let Conductor = function (service, config) {
   this.service = service
   this.config = config
 
+  this.timesStarted = 0
+
   let watcher = DI.container.get(DI.FILETYPES.Watcher)
   watcher.watch()
 
   this.orchestrate = () => {
     return Promise.delay(100)
       .then(() => {
-        logger.info('waiting for file changes...')
-        return watcher.waitForFileChanges()
+        if (this.timesStarted === 0) {
+          this.timesStarted++
+          logger.info('register dapp...')
+          return Promise.delay(0)
+        } else {
+          this.timesStarted++
+          logger.info('waiting for file changes...')
+          return watcher.waitForFileChanges()
+        }
       })
       .then(() => {
         logger.info('finished watching files...')
